3e0ca90d3e27fd0171ebd8b75cd02180cddc5f13
Adenike Adeyemi <adenikeadeyemi@Adenike-Adeyemis-MacBook-Pro.local>
2016-08-21 19:25:24 -0400

    fixing video display

--- assets/javascript/karaoke.js
+++ assets/javascript/karaoke.js
@@ -19,7 +19,7 @@
       function onYouTubeIframeAPIReady() {
       	player = new YT.Player('player', {
       		height: '360',
-      		width: '640',
+      		width: '480',
           	videoId: localStorage.videoID, //<---determines video
             playerVars:{
               'showInfo': 0,
@@ -40,11 +40,11 @@
       // 5. The API calls this function when the player's state changes. Ex: playing, buffering, stopped, ending, etc...
       var done = false;
       function onPlayerStateChange(event) {
-      	/*
+      	
         if (event.data == YT.PlayerState.PLAYING && !done) {
       		setTimeout(stopVideo, 6000);
       		done = true;
-      	}*/
+      	}
       }
       
       /*
